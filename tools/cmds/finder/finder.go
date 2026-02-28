669384f825cb4e77a5e681276a6b6bac204fa159
66james99 <j.e.hammick@gmail.com>
2026-02-28 14:44:15 +0000

    refactored to move obtaining cmd line args and flags into functions outside main

--- tools/cmds/finder/finder.go
+++ tools/cmds/finder/finder.go
@@ -4,10 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
-func main() {
-	// Define flags
+func parseFlags() (*bool, *bool, *string, *string) {
 	dryRun := flag.Bool("dryrun", false, "Indicates no changes should be made, only report what would have been changed")
 	dateFromExif := flag.Bool("date_from_exif", false, "Indicates if date should be extracted from EXIF (images only)")
 	rootDir := flag.String("rootdir", "", "Path to the root directory (images only)")
@@ -23,30 +23,53 @@ func main() {
 	}
 
 	flag.Parse()
+	return dryRun, dateFromExif, rootDir, pattern
+}
 
-	// Check for positional argument (source)
+func parseOptions() string {
 	args := flag.Args()
 	if len(args) != 1 {
 		fmt.Println("Error: source parameter is required")
 		flag.Usage()
 		os.Exit(1)
 	}
+	return args[0]
+}
 
-	source := args[0]
+func validateFlags(source string) {
+	validFlagsBySource := map[string][]string{
+		"images":  {"dryrun", "date_from_exif", "rootdir", "pattern"},
+		"tickets": {"dryrun"},
+		"info":    {"dryrun"},
+	}
 
-	// Validate source
-	switch source {
-	case "images":
-		// Valid
-	case "tickets", "info":
-		if *dateFromExif || *rootDir != "" || *pattern != "" {
-			fmt.Println("Error: flags --date_from_exif, --rootdir, and --pattern are only valid when source is 'images'")
-			os.Exit(1)
+	allowedFlags, sourceIsValid := validFlagsBySource[source]
+	if !sourceIsValid {
+		knownSources := make([]string, 0, len(validFlagsBySource))
+		for k := range validFlagsBySource {
+			knownSources = append(knownSources, k)
 		}
-	default:
-		fmt.Printf("Error: invalid source '%s'. Must be one of: images, tickets, info\n", source)
+		fmt.Printf("Error: invalid source '%s'. Must be one of: %s\n", source, strings.Join(knownSources, ", "))
 		os.Exit(1)
 	}
+
+	allowedSet := make(map[string]struct{}, len(allowedFlags))
+	for _, f := range allowedFlags {
+		allowedSet[f] = struct{}{}
+	}
+
+	flag.Visit(func(f *flag.Flag) {
+		if _, allowed := allowedSet[f.Name]; !allowed {
+			fmt.Printf("Error: flag --%s is not valid for source '%s'\n", f.Name, source)
+			os.Exit(1)
+		}
+	})
+}
+
+func main() {
+	dryRun, dateFromExif, rootDir, pattern := parseFlags()
+	source := parseOptions()
+	validateFlags(source)
 
 	fmt.Printf("Source: %s\nDryrun: %v\n", source, *dryRun)
 	if source == "images" {
